5661e2214083b59569b8661eb847a26c31b6cb5d
Martín Anzorena <martin.anzorena@gmail.com>
2015-07-12 18:17:51 -0300

    Course Project 1 (plots 1 & 2)

---
+++ plot1.R
@@ -0,0 +1,38 @@
+# load 10 lines of data
+data <- read.table("data/household_power_consumption.txt",
+                   header = TRUE,
+                   sep = ";",
+                   nrows = 10,
+                   na.strings = '?')
+
+# get column classes
+colClasses <- sapply(data, class)
+
+# read data
+data <- read.table("data/household_power_consumption.txt",
+                   header = TRUE,
+                   sep = ";",
+                   colClasses = colClasses,
+                   na.strings = '?')
+
+# Parse date/time into datetime and delete unnecesary columns
+data$DateTime <- strptime(paste(as.character(data[,1]), as.character(data[,2])),
+                   '%d/%m/%Y %H:%M:%S')
+
+# Delete unnecessary columns 
+data <- data[,3:10]
+
+# Keep only records from 2007-02-01 and 2007-02-02
+data <- data[as.Date(data$DateTime) >= as.Date('2007-02-01')
+             & as.Date(data$DateTime) < as.Date('2007-02-03'),]
+
+png(filename = "plot1.png",
+    width = 480,
+    height = 480)
+
+hist(data$Global_active_power,
+     col = "red",
+     main = "Global Active Power",
+     xlab = "Global Active Power (kilowatts)")
+
+dev.off()
