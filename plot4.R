4e7bc3118f356eb862194ae97143ef7189cd6e4c
Martín Anzorena <martin.anzorena@gmail.com>
2015-07-12 23:47:29 -0300

    Fix plot 4

---
+++ plot4.R
@@ -0,0 +1,80 @@
+# load 10 lines of data
+data <- read.table("data/household_power_consumption.txt",
+                   header = TRUE,
+                   sep = ";",
+                   nrows = 10,
+                   na.strings = '?')
+
+# get column classes
+colClasses <- sapply(data, class)
+
+# read data
+data <- read.table("data/household_power_consumption.txt",
+                   header = TRUE,
+                   sep = ";",
+                   colClasses = colClasses,
+                   na.strings = '?')
+
+# Parse date/time into datetime and delete unnecesary columns
+data$DateTime <- strptime(paste(as.character(data[,1]), as.character(data[,2])),
+                          '%d/%m/%Y %H:%M:%S')
+
+# Delete unnecessary columns 
+data <- data[,3:10]
+
+# Keep only records from 2007-02-01 and 2007-02-02
+data <- data[as.Date(data$DateTime) >= as.Date('2007-02-01')
+             & as.Date(data$DateTime) < as.Date('2007-02-03'),]
+
+# set plot size and file name
+png(filename = "plot4.png",
+    width = 480,
+    height = 480)
+
+# set 4x4 plots in canvas
+par(mfrow = c(2,2))
+
+# plot 1,1
+plot(data$DateTime,
+     data$Global_active_power,
+     type = "s",
+     ylab = "Global active power",
+     xlab = "")
+
+# plot 1,2
+plot(data$DateTime,
+     data$Voltage,
+     type = "s",
+     xlab = "datetime",
+     ylab = "Voltage")
+
+# plot 2,1
+
+plot(data$DateTime,
+     data$Sub_metering_1,
+     type = "s",
+     ylab = "Energy sub metering",
+     xlab = "")
+
+lines(data$DateTime,
+      data$Sub_metering_2, col = "red")
+
+lines(data$DateTime,
+      data$Sub_metering_3, col = "blue")
+
+# Add legend
+legend("topright", # position
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), # texts
+       lty = c(1,1,1), # set lines
+       col = c("black", "red", "blue"), # set lines colors
+       bty = "n" # borderless frame
+       ) 
+
+# plot 2,2
+plot(data$DateTime,
+     data$Global_reactive_power,
+     type = "s",
+     ylab = "Global_reactive_power",
+     xlab = "datetime")
+
+dev.off()
